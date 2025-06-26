98203981c8bf9d1c387dca4773e7114f0d465c7f
kamalyanaram <aramaramyants@gmail.com>
2025-06-26 18:06:32 +0400

    init: Basic Typescript in props(array, object, string, number)

---
+++ react-typescript-demo/src/App.tsx
@@ -0,0 +1,37 @@
+import './App.css';
+import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
+
+function App() {
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne'
+  }
+
+  const nameList = [
+    {
+      first: 'Bruce',
+      last: 'Wayne'
+    },
+    {
+      first: 'Clark',
+      last: 'Kent'
+    },
+    {
+      first: 'Peter',
+      last: 'Parker'
+    }
+  ]
+  return (
+    <>
+      <div className='App'>
+        <Greet name={'Aram'} messageCount={20} isLoggedIn={false} />
+        <Person name={personName}/>
+        <PersonList names={nameList}/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ react-typescript-demo/src/components/Person.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type personProps = {
+  name: {
+    first: string;
+    last: string;
+  };
+};
+
+export const Person = (props: personProps) => {
+  return (
+    <div>
+      {props.name.first} {props.name.last}
+    </div>
+  );
+};
