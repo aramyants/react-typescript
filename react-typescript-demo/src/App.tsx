313408ebd2a48c59f205e86014cfe5b3c69fb4d1
kamalyanaram <aramaramyants@gmail.com>
2025-07-01 11:03:44 +0400

    chore: Style Props

--- react-typescript-demo/src/App.tsx
+++ react-typescript-demo/src/App.tsx
@@ -1,41 +1,12 @@
 import './App.css';
-import { Button } from './components/Button';
-import { Greet } from './components/Greet';
-import { Heading } from './components/Heading';
-import { Input } from './components/Input';
-import { Oscar } from './components/Oscar';
-import { Person } from './components/Person';
-import { PersonList } from './components/PersonList';
-import { Status } from './components/Status';
+import { Container } from './components/Container';
 
 function App() {
-  const personName = {
-    first: 'Bruce',
-    last: 'Wayne',
-  };
 
-  const nameList = [
-    {
-      first: 'Bruce',
-      last: 'Wayne',
-    },
-    {
-      first: 'Clark',
-      last: 'Kent',
-    },
-    {
-      first: 'Peter',
-      last: 'Parker',
-    },
-  ];
   return (
     <>
       <div className="App">
-        <Button
-          handleClick={(event, id) => console.log('Button clicked', event, id)}
-        />
-        <Input  handleChange={(event) => console.log('Input changed', event)} />
-
+        <Container styles={{border: '1px solid white', padding: '1rem'}} />
       </div>
     </>
   );
